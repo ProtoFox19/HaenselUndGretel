55f5b2b0fd821f49cc8df16432f35bf2a73e8881
MaxFlux <rothmax91@gmail.com>
2017-01-18 19:13:50 +0100

    Sprechanimation "fertig" ;)

---
+++ Assets/Scripts/AllTexts.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class AllTexts : MonoBehaviour {
+
+
+	// Scene 1
+
+	public string welcome = "Benutze das Steuerkreuz zur Bewegung ... ";
+
+	public string move = "... Nehme dir ein wenig Zeit, dich umzuschauen";
+
+	public string where = "Du befindest dich gerade in einem Traum";
+
+	public string live = "Deine Spielerfigur verliert mit der Zeit an Licht ... ";
+
+	public string live2 = "... dieses Licht stellt dein Leben dar";
+
+	public string live3 = "... lasse es nicht erlischen!\nSammle dazu die schwebenden Lichter ein";
+
+	public string shadows = "Pass gut auf die Schatten auf!\n Sie entziehen dir Licht !";
+
+	public string tip = "Siehe dich immer gut um und nutze deine Umgebung";
+
+	public string collapse = "Was ist das? Was passiert?\n Es bricht alles zusammen ?!?!";
+
+	public string fall = "Nein ... Neeeiinn ... Ich kann nicht ...\n mein Traum zerfällt !!";
+
+	private Text text;
+
+	public string [] messages;
+
+	public float [] speaker;
+
+	public int counter = 0;
+
+	private int speakerCounter = 0;
+
+	public bool scene1 = false;
+
+
+
+
+	public float gretel = 1;
+
+	public float haensel = 2;
+
+	public float mom = 3;
+
+	public float dad = 4;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		text = GetComponent<Text> ();
+
+		if (scene1 == true ) {
+
+			messages = new string[10];
+
+			speaker = new float[10];
+
+
+			messages [0] = welcome;
+			speaker [0] = gretel;
+
+			messages [1] = move;
+			speaker [1] = haensel;
+
+			messages [2] = where;
+			speaker [2] = gretel;
+
+			messages [3] = live;
+			speaker [3] = haensel;
+
+			messages [4] = live2;
+			speaker [4] = gretel;
+
+			messages [5] = live3;
+
+			messages [6] = shadows;
+
+			messages [7] = tip;
+
+			messages [8] = collapse;
+
+		}
+
+	}
+
+	public float currentSpeaker () {
+
+
+		return speaker [speakerCounter];
+
+	}
+
+	public void countSpeaker () {
+
+		speakerCounter++;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text.text = messages [counter];
+	}
+
+	public void count () {
+
+		if (counter < messages.Length) {
+
+			text.text = messages [counter];
+
+			counter ++;
+		}
+	}
+}
---
+++ Assets/Scripts/TextInteraktion.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextInteraktion : MonoBehaviour {
+
+	public bool gretel = false; // 1
+
+	public bool haensel = false; // 2
+
+	public bool mom = false; // 3
+
+	public bool dad = false; // 4
+
+
+
+	public GameObject haenseltest;
+
+	public GameObject greteltest;
+
+
+
+
+	private void OnMouseDown() {
+
+		GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts> ().countSpeaker ();
+
+
+		GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts> ().count ();
+
+
+	}
+
+
+
+
+	/*void Update () {
+
+
+		if (gretel == true) {
+
+			// Aktiviere Gretel, wenn spricht
+			if (GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts>().currentSpeaker() == 1) {
+
+				gameObject.SetActive (true);
+
+
+				Debug.Log (GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts> ().currentSpeaker ());
+
+
+
+			} else { gameObject.SetActive (false); }
+
+
+		}
+
+
+		if (haensel == true) {
+
+			// Aktiviere haensel, wenn spricht
+			if (GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts>().currentSpeaker() == 2) {
+
+				gameObject.SetActive (true);
+
+				Debug.Log (GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts> ().currentSpeaker ());
+
+
+
+			} else { gameObject.SetActive (false); }
+
+
+		}
+
+	}*/
+
+
+
+	void Update () {
+
+			// Aktiviere haensel, wenn spricht
+			if (GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts>().currentSpeaker() == 1) {
+
+				//gameObject.SetActive (true);
+
+				greteltest.SetActive (true);
+				haenseltest.SetActive (false);
+
+			}
+
+			// Aktiviere haensel, wenn spricht
+			if (GameObject.FindGameObjectWithTag ("AllText").GetComponent<AllTexts>().currentSpeaker() == 2) {
+
+				//gameObject.SetActive (true);
+
+				haenseltest.SetActive (true);
+				greteltest.SetActive (false);
+
+			}
+
+		}
+
+
+}
